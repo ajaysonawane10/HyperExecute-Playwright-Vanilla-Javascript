cfc98919477faa186c4cf4a71c5ed66ae62b077a
ajaysonawane10 <ajaysonawane10@yahoo.com>
2026-03-03 22:28:14 +0530

    Remove redundant waitForTimeout in test_1.spec.js
    Comment out unnecessary waitForTimeout calls in test.

--- tests/test_1.spec.js
+++ tests/test_1.spec.js
@@ -7,19 +7,19 @@ test.describe('PlayWright Vanilla JS - 1', () => {
     const message = 'Welcome to TestMu AI';
 
   await page.goto('https://www.testmuai.com/selenium-playground/');
-  await page.waitForTimeout(3000); // 
+  //await page.waitForTimeout(3000); // 
 
   await page.getByText('Simple Form Demo').click();
-  await page.waitForTimeout(3000); // 
+  //await page.waitForTimeout(3000); // 
 
   await expect(page).toHaveURL(/simple-form-demo/);
-  await page.waitForTimeout(3000); // 
+  //await page.waitForTimeout(3000); // 
 
   await page.getByPlaceholder('Please enter your Message').fill(message);
-  await page.waitForTimeout(3000); // 
+  //await page.waitForTimeout(3000); // 
 
   await page.getByRole('button', { name: 'Get Checked Value' }).click();
-  await page.waitForTimeout(3000); // 
+  //await page.waitForTimeout(3000); // 
 
   await expect(page.locator('#message')).toHaveText(message);
   
